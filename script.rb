fe213b5f714733208f8b21c440c444b138f388f4
Gustavo <gusta.front@gmail.com>
2022-07-14 13:41:55 -0300

    Refactored after rereading the fundamentals of Ruby

--- script.rb
+++ script.rb
@@ -1,16 +1,16 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(string, words)
-    array_of_substrings = []
-    string.split(' ').each do |string|
-        array_of_substrings << words.filter_map { |word| word if string.downcase.include?(word) }
-    end 
-    array_of_substrings.flatten!
-    hash_of_substrings = array_of_substrings.reduce(Hash.new(0)) do |result, substring|
-        result[substring] += 1
-        result
+def substrings(string, dictionary)
+    valid_substrings = Hash.new(0)
+    dictionary.each do |substring|
+        string.downcase.split(" ").each do |word|
+            if word.include?(substring)
+                valid_substrings[substring] += 1
+            end
+        end
     end
-    p hash_of_substrings
+    p valid_substrings
 end
 
+substrings("below", dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
